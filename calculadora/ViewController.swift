f5ee02925d574bc98cc5f8a636e59484bb59dda9
Paula Tomasi <paula.andrade@cwi.com.br>
2019-05-29 13:32:58 -0300

    switches with enum

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -12,32 +12,33 @@ class ViewController: UIViewController {
     var calculadora = Calculadora()
     var firstNumber: String = "0"
     var secondNumber: String = "0"
-    var operation: String = ""
+    var operation: Operations = .noOperation
     var hasResponse: Bool = false
 
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func onButtonPress(_ sender: UIButton) {
         let displayText = displayLabel.text ?? ""
+        let buttonTitle = sender.currentTitle ?? ""
         
-        if let buttonTitle = sender.currentTitle {
-            switch(buttonTitle) {
-            case "AC":
+        if let selectedOperation = Operations(rawValue: sender.tag) {
+            switch(selectedOperation) {
+            case .clear:
                 clear()
-            case "√x", "x²":
-                operation = buttonTitle
+            case .sqrt, .pow:
+                operation = selectedOperation
                 calculate()
-            case "÷", "x", "-", "+":
+            case .div, .times, .minus, .plus:
                 hasResponse = false
-                operation = buttonTitle
-            case "=":
+                operation = selectedOperation
+            case .equals:
                 calculate()
             default:
                 if hasResponse {
                     clear()
                     firstNumber = buttonTitle
                     displayLabel.text = firstNumber
-                } else if operation.isEmpty {
+                } else if operation == .noOperation {
                     firstNumber = firstNumber == "0" ? buttonTitle : displayText + buttonTitle
                     displayLabel.text = firstNumber
                 } else {
@@ -52,28 +53,28 @@ class ViewController: UIViewController {
         let number = firstNumber.floatValue
         let number2 = secondNumber.floatValue
         var response: Float = 0.0
-        
+
         switch(operation) {
-        case "√x":
+        case .sqrt:
             response = calculadora.squareRoot(number: number)
-        case "x²":
+        case .pow:
             response = calculadora.pow(number: number)
-        case "÷":
+        case .div:
             if number2 == 0.0 {
                 response = Float.nan
             } else {
                 response = calculadora.div(firstNumber: number, secondNumber: number2)
             }
-        case "x":
+        case .times:
             response = calculadora.times(firstNumber: number, secondNumber: number2)
-        case "-":
+        case .minus:
             response = calculadora.minus(firstNumber: number, secondNumber: number2)
-        case "+":
+        case .plus:
             response = calculadora.plus(firstNumber: number, secondNumber: number2)
         default:
             response = number
         }
-        
+
         clear()
         firstNumber = response.toString
         displayLabel.text = firstNumber
@@ -83,9 +84,20 @@ class ViewController: UIViewController {
     func clear() {
         firstNumber = "0"
         secondNumber = "0"
-        operation = ""
+        operation = .noOperation
         displayLabel.text = "0"
         hasResponse = false
     }
 }
 
+enum Operations: Int {
+    case noOperation = 0
+    case clear = 1
+    case sqrt = 2
+    case pow = 3
+    case div = 4
+    case times = 5
+    case minus = 6
+    case plus = 7
+    case equals = 8
+}
